89a4d715c6d654e8cf323ec6f900a5a4d59635c3
Javier <javier.gauna147@gmail.com>
2026-06-18 21:11:04 -0300

    """ ABM de usuarios: nuevas páginas y lógica de negocio
    Se agregaron UsuariosLista.aspx y UsuariosFormulario.aspx con sus archivos de código y diseñador. Se añadió el enlace a Usuarios en la navegación principal. Se implementó la lógica para listar, agregar, editar y eliminar usuarios, incluyendo la clase UsuarioNegocio en la capa de negocio. Se actualizaron los archivos de proyecto y binarios.
    """

---
+++ Comercio-Web/UsuariosFormulario.aspx.designer.cs
@@ -0,0 +1,13 @@
+namespace Comercio_Web
+{
+    public partial class UsuariosFormulario
+    {
+        protected global::System.Web.UI.WebControls.Label lblTitulo;
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+        protected global::System.Web.UI.WebControls.TextBox txtUsuarioLogin;
+        protected global::System.Web.UI.WebControls.TextBox txtContraseña;
+        protected global::System.Web.UI.WebControls.CheckBox chkAdmin;
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
+        protected global::System.Web.UI.WebControls.Button btnEliminar;
+    }
+}
---
+++ Comercio-Web/UsuariosLista.aspx.designer.cs
@@ -0,0 +1,7 @@
+namespace Comercio_Web
+{
+    public partial class UsuariosLista
+    {
+        protected global::System.Web.UI.WebControls.GridView dgvUsuarios;
+    }
+}
